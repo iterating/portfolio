66d9db1df226d863b6da89743addef9d4f8fee9a
Jonathan Young <6834401+iterating@users.noreply.github.com>
2025-03-18 20:55:15 -0700

    gh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './assets/css/bootstrap.min.css'
 import './css/App.css'
 import './css/responsive.css'
+import React from 'react'
 import Global from './components/Global'
 
 function App() {
